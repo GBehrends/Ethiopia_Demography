9489a2a28b8426fcc07fcae67627a0058f6eda1f
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-01-26 14:24:14 -0600

    Add files via upload
    # Using satsuma2 to align congeneric species' assemblies at the scaffold level to chromosomal assemblies of the next most closely related species to improve the use of MSMC2 across taxa. 
    Step 1: 01_rename_chrom_satsuma.sh renames chromosomes consistently across taxa. It will take the file "chrom_list.txt" which is a list of assemblies to be renamed. You should have a list of chromosomes for each assembly and what you want them to be renamed as. An example for the latter is provided as "chrom_example.txt". 
    
    Step 2: 02_run_satsuma.sh. This step takes a list of target assemblies and a list of query assemblies that will align to the target assemblies. It will run satsuma2 and output a new assembly titled likeso "${query}_pseudo.fasta".

---
+++ 01_rename_chrom_satsuma.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#SBATCH --job-name=rename_chrom
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+#SBATCH --partition=nocona
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=1
+
+# Iterate through list of assembly prefixes
+array=$( head -n${SLURM_ARRAY_TASK_ID} chrom_list.txt | tail -n1 )
+
+# Unzip assembly
+gunzip ${array}_genomic.fna.gz 
+
+# Replace chromosome names with simpler list of names for uniformity amongst species 
+while read n k; do sed -i "/$k/ c\>$n" ${array}_genomic.fna; done < ${array}_chrom_name.txt
+
+# Rezip the assembly to save space
+gzip ${array}_genomic.fna
+
+
+
+
+
