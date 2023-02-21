f51adfcbe585983d996a5a6466395deaef1296a6
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-02-21 10:32:44 -0600

    Update 01_Export_Het_VCF.sh

--- 06_Heterozygosity/01_Export_Het_VCF.sh
+++ 06_Heterozygosity/01_Export_Het_VCF.sh
@@ -12,16 +12,22 @@
 workdir=?
 
 # define input files from helper file during genotyping
-sample=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/msmc/vcf/helper1.txt | tail -n1 )
+sample=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/het/samplelist.txt | tail -n1 )
 
 # Cat large enough vcfs to make a vcf without sex chromosomes
-for i in $(ls ${workdir}/msmc/vcf/${sample}/*vcf); do 
+for i in $(ls ${workdir}/msmc/vcf/${sample}/*vcf | grep -v "Chromosome_Z" | grep -v "Chromosome_W"); do 
 cat ${i} >> ${workdir}/het/${sample}.vcf; done
 
 # Add sex chromosomes and make a different file
 for i in Z W; do 
-zcat ${workdir}/msmc/vcf/${sample}/excluded_chrom/Chromosome_${i}.vcf.gz >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf
-zcat ${workdir}/msmc/vcf/${sample}/excluded_chrom/PseudoChromosome_${i}.vcf.gz >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf; done
+
+# If sex chromosomes were too small and gzipped 
+zcat ${workdir}/msmc/vcf/${sample}/excluded_chrom/Chromosome_${i}.vcf.gz >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf;
+zcat ${workdir}/msmc/vcf/${sample}/excluded_chrom/PseudoChromosome_${i}.vcf.gz >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf; 
+
+# If sex chromosomes were not too small and were kept 
+cat ${workdir}/msmc/vcf/${sample}/Chromosome_${i}.vcf >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf;
+cat ${workdir}/msmc/vcf/${sample}/PseudoChromosome_${i}.vcf >> ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf; done
 
 cat ${workdir}/msmc/vcf/${sample}/${sample}_sex.vcf ${workdir}/het/${sample}.vcf > ${workdir}/het/${sample}_sex.vcf
 
