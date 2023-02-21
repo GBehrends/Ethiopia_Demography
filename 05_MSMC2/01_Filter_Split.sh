6c3639b5608e7456f070d920956b81f2c9ee7120
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-02-21 09:43:20 -0600

    Update 01_Filter_Split.sh

--- 05_MSMC2/01_Filter_Split.sh
+++ 05_MSMC2/01_Filter_Split.sh
@@ -12,35 +12,26 @@
 conda activate vcftools
 
 # Define the working directory 
-workdir=/lustre/scratch/gbehrend/Demog_ETH
+workdir=?
 
-# define input files from helper file during genotyping
-sample=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/msmc/vcf/helper1.txt | tail -n1 )
-depth=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/msmc/vcf/helper2.txt | tail -n1 )
+# define input file prefixes from helper file during genotyping
+sample=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/msmc/vcf/samplelist.txt | tail -n1 )
+
+# define the max depth of coverage threshold for each sample in a list  
+depth=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/msmc/vcf/depthlist.txt | tail -n1 )
 
 # Make species sample directory to place all chromosome vcfs into 
-mkdir ${workdir}/msmc/vcf/${sample} 
+#mkdir ${workdir}/msmc/vcf/${sample} 
 
 # Filter species sample vcf for MSMC2 and simplify it to contain the desired columns 
-vcftools --gzvcf ${workdir}/02_vcf/${sample}.vcf.gz --minDP 6 --maxDP ${depth} --max-missing 1 --max-alleles 2  --remove-indels --recode --recode-INFO-all -c | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' > ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf
+#vcftools --gzvcf ${workdir}/02_vcf/${sample}.vcf.gz --minDP 6 --maxDP ${depth} --max-missing 1 --max-alleles 2  --remove-indels --recode --recode-INFO-all -c | bcftools q
+uery -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' > ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf
 
 # Establish list of chromosomes inside of species sample vcf 
-cut -f1 ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf | sort | uniq > ${workdir}/msmc/vcf/${sample}/chrom_list.txt
+zcat ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf.gz | cut -f1 | sed 's/ //g' | sort | uniq > ${workdir}/msmc/vcf/${sample}/chrom_list.txt
 
-# Make chromosomal vcfs 
-for i in $(cat ${workdir}/msmc/vcf/${sample}/chrom_list.txt); do grep "${i}" ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf | cut -f2,3,4,5 >  ${workdir}/msmc/vcf/${sample}/${i}.vcf ; done 
+# Create helper file with sample name to pair with the chromlist
+for i in $(cat ${workdir}/msmc/vcf/${sample}/chrom_list.txt); do echo ${sample} >> ${workdir}/msmc/vcf/${sample}/samp_list.txt; done 
 
 # Create directory to store vcfs that will not be included in further analyses 
 mkdir ${workdir}/msmc/vcf/${sample}/excluded_chrom
-
-# Include only vcfs > 1Mbp
-for i in $(cat ${workdir}/msmc/vcf/${sample}/chrom_list.txt); do if (("$(tail -n 1 ${workdir}/msmc/vcf/${sample}/${i}.vcf | cut -f1)" < 100000)); then mv ${workdir}/msmc/vcf/${sample}/${i}.vcf ${workdir}/msmc/vcf/${sample}/excluded_chrom/${i}.vcf; fi; done
-
-# Move Sex Chromosomes to excluded folder
-for i in W Z; do mv ${workdir}/msmc/vcf/${sample}/Chromosome_${i}.vcf ${workdir}/msmc/vcf/${sample}/excluded_chrom/Chromosome_${i}.vcf; done 
-
-# Move whole species vcf to excluded folder as well 
-mv ${workdir}/msmc/vcf/${sample}/${sample}.simple.vcf  ${workdir}/msmc/vcf/${sample}/excluded_chrom/${sample}.simple.vcf 
-
-# Gzip the excluded chromosomes to save space 
-for i in $(ls ${workdir}/msmc/vcf/${sample}/excluded_chrom); do gzip ${workdir}/msmc/vcf/${sample}/excluded_chrom/${i}; done 
