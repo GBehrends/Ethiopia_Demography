b0bd50a20f3ed117e25fb7f2663c43da358d901e
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-02-23 15:48:52 -0600

    Update 01_Call_Variants.sh

--- 04_Genotyping/01_Call_Variants.sh
+++ 04_Genotyping/01_Call_Variants.sh
@@ -5,18 +5,18 @@
 #SBATCH --nodes=1 --ntasks=8
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=2-17 
+#SBATCH --array=1-17
 
 # Activate conda environment containing bcftools
 . ~/conda/etc/profile.d/conda.sh
 conda activate vcftools
 
 # Define main working directory
-workdir=/lustre/scratch/gbehrend/Demog_ETH
+workdir=?
 
 # Provide helper file with species, respective reference assemblies, and --max-depth threshold 
-species=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/helper1.txt | tail -n1 )
-max=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/helper2.txt | tail -n1 )
+species=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/samplelist | tail -n1 )
+max=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/depthlist | tail -n1 )
 ref=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/helper3.txt | tail -n1 )
 
 # Unzip the reference assembly 
