5be40123a645eabe173a56a7259e9a1a19184e28
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-04-03 12:30:00 -0500

    Update 05_Make_Helpers.sh

--- 05_MSMC2/05_Make_Helpers.sh
+++ 05_MSMC2/05_Make_Helpers.sh
@@ -6,10 +6,28 @@
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
 
-#Establish working directory 
+# Establish working directory. 
 workdir=/lustre/scratch/gbehrend/Demog_ETH/msmc
 
-for i in $(cat sampleslist.txt); do 
-  cat ${sample}_demography/helper1.txt >> ${workdir}/msmc/helper1.txt;
-  cat ${sample}_demography/helper2.txt >> ${workdir}/msmc/helper2.txt; done
+# Iterate through each sample from each species. 
+for x in $(cat samplelist); 
+do cd /lustre/scratch/gbehrend/Demog_ETH/msmc/${x}_demography/; \
+
+  # Iterate through list of all of all directories per sample and insert *txt on the end to 
+  # incorporate all scaffolds in each directory. 
+  for i in $(find ~+ -type d | sed '1d' | sed 's,\./,,g'); 
+  
+    # Making a list of input files (scaffolds).  
+    do echo "${i}/*txt" >> /lustre/scratch/gbehrend/Demog_ETH/msmc/helper1.txt; 
+    
+    # Making a list of output files (Final coalesence rate tables). These will go in the 
+    # main species demography directory. 
+    echo "${i}" | sed "s,${x}/,,g" >> /lustre/scratch/gbehrend/Demog_ETH/msmc/helper2.txt; done
+    
+    # Exit species demography directory to begin next species. 
+    cd /lustre/scratch/gbehrend/Demog_ETH/msmc/;
+   
+   done
+
+
   
