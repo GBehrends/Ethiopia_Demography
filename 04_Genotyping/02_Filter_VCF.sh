69c545a730fa297619224e80b827036ef25c03ad
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-04-11 17:44:54 -0500

    Update 02_Filter_VCF.sh

--- 04_Genotyping/02_Filter_VCF.sh
+++ 04_Genotyping/02_Filter_VCF.sh
@@ -1,24 +1,23 @@
-
 #!/bin/sh
 #SBATCH --chdir=./
-#SBATCH --job-name=filt_vcf 
-#SBATCH --nodes=1 --ntasks=12
+#SBATCH --job-name=cat_vcf  
+#SBATCH --nodes=1 --ntasks=4
 #SBATCH --partition=quanah
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-17
+#SBATCH --array=1-?
 
-
-# activate conda environment with vcftools 
+# Load environment 
 . ~/conda/etc/profile.d/conda.sh
-conda activate vcftools
+conda activate vcftools 
 
-# define input files from helper file during genotyping
-sample=$( head -n${SLURM_ARRAY_TASK_ID} samplelist | tail -n1 )
-depth=$( head -n${SLURM_ARRAY_TASK_ID} depthlist | tail -n1 )
-
-# define main working directory
+# Define the working directory 
 workdir=?
 
-# Filter for MSMC and Observed Heterozygosity
-vcftools --gzvcf ${workdir}/02_vcf/${sample}.vcf.gz  --minDP 6 --maxDP ${depth} --max-missing 1 --max-alleles 2   --remove-indels --recode --recode-INFO-all -c | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' > ${workdir}/msmc/vcf/${sample}.vcf 
+# define input files from helper file during genotyping
+sample=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/samplelist| tail -n1 )
+depth=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/02_vcf/depthlist | tail -n1 )
+
+# Filter using vcftools
+vcftools --gzvcf ${workdir}/02_vcf/${sample}.vcf.gz --minDP 6 --maxDP ${depth} --max-missing 1 --max-alleles 2  --remove-indels --recode --recode-INFO
+-all --out ${workdir}/03_vcf/${sample} 
