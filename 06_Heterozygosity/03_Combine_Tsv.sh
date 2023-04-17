fcc402af96c929f6b14f4dfae6a3a45efc3bf5c6
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-04-17 13:20:21 -0500

    Update 03_Combine_Tsv.sh

--- 06_Heterozygosity/03_Combine_Tsv.sh
+++ 06_Heterozygosity/03_Combine_Tsv.sh
@@ -1,7 +1,30 @@
-# Run the following command interactively to combine all chromosomal het calculations by species 
+# Run the following interactively to combine all chromosomal heterozygosity calculations by species
+# and then sort by chromosome and chromosome position 
+#!/bin/sh
+
+# run on an interactive session 
+interactive -c 1 
+
+# Define working directory 
 workdir=<main directory> 
 
+# Combine tsv files into species heterozygosity tables 
 for i in $(cat samplelist.txt); 
   do echo "${i}" > ${workdir}/het/${i}_het.tsv; 
   for x in $(ls ${i});
     do  tail -n +2 ${i}/${x} >> ${workdir}het/${i}_het.tsv; done ; done 
+ 
+ 
+# Sorting the heterozygosity tables 
+# Iterate through each chromosome and place the species header in the sorted file  
+for i in $(cat samplelist.txt); do     
+  echo "${i}" > ${workdir}/${i}_sorted.tsv;
+
+  # Make single digit chromosome numbers begin with 0 for easy sorting 
+  for x in {1..9}; 
+    do sed -i "s/_${x}:/_0${x}:/g" ${workdir}/${i}_het.tsv; done 
+  
+  # Remove the header on the orignal file before sorting it and placing it in
+  # the sorted file 
+  sed '1d' ${workdir}/${i}_het.tsv | sort -k1  >> ${workdir}/${i}_sorted.tsv; done
+ 
