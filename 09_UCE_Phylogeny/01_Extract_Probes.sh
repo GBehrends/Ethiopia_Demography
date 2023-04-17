6b3d4284a4452b8b1e471b57d766fc6b20a3ef6c
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-04-17 15:08:36 -0500

    Update 01_Extract_Probes.sh

--- 09_UCE_Phylogeny/01_Extract_Probes.sh
+++ 09_UCE_Phylogeny/01_Extract_Probes.sh
@@ -7,20 +7,32 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-<number of reference assemblies used> 
 
-# Activate conda environment containing Phyluce-1.7.2 
+# Activate conda environment containing Phyluce-1.7.2 and faToTwoBit
 . ~/conda/etc/profile.d/conda.sh
 conda activate phyluce-1.7.2 
 
 # Define the working directory 
 workdir=/lustre/scratch/gbehrend/Demog_ETH/UCEs
 
+# Define path to reference assembllies 
+ref_path=<>
+
 # Establish array list 
 ref=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/ref/references | tail -n1 )
+
+# Copy reference assembly to 2bit folder 
+mkdir -p ${workdir}/ref/${ref}/
+cp ${ref_path}/${ref}.fasta ${workdir}/ref/${ref}/
+
+# Convert each reference assembly into 2bit format. Make sure that you have your 
+faToTwoBit ${workdir}/ref/${ref}/${ref}.fasta ${workdir}/ref/${ref}/${ref}.2bit
+mv ${workdir}/ref/${ref}/${ref}.2bit ${workdir}/ref/${ref}.2bit
+rm -r ${workdir}/ref/${ref}/
 
 # Determining UCE sequences in reference assemblies using UCE probe set
 phyluce_probe_run_multiple_lastzs_sqlite  --db ${workdir}/reference_probes/${ref}.db \
 --output ${workdir}/reference_probes/${ref}_lastz --scaffoldlist ${ref} \
---genome-base-path${workdir}/ref --probefile ${workdir}/reference_probes/uce-5k-probes.fasta --cores 12
+--genome-base-path ${workdir}/ref --probefile ${workdir}/reference_probes/uce-5k-probes.fasta --cores 12
 
 # Make config file for current reference genome 
 echo "[scaffolds]" > ${workdir}/reference_probes/${ref}.config 
